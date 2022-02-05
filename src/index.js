0243d0a8485041af83b7d3213096fd4003c07cb5
Chetan <chetansinghbisht@cedcoss.com>
2022-02-05 13:44:54 +0530

    task45

--- src/index.js
+++ src/index.js
@@ -177,3 +177,8 @@ const myArray = [["John", 23], ["cat", 2]];
 
 // Only change code below this line
 myArray.push(["dog", 3]); //task44
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+var removedFromMyArray = myArray.pop(); //task45

120274c54d74ce249ab6379f096ef1b5b691a723
Chetan <chetansinghbisht@cedcoss.com>
2022-02-05 13:16:22 +0530

    task36

--- src/index.js
+++ src/index.js
@@ -136,3 +136,8 @@ const lastName = "Lovelace";
 
 // Only change code below this line
 const thirdLetterOfLastName = lastName[2]; // Change this line //task35
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line //task36

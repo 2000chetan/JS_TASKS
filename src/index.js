657126f560e59bdb58e31e3af5218675d14caf64
Chetan <chetansinghbisht@cedcoss.com>
2022-02-05 15:03:27 +0530

    task55

--- src/index.js
+++ src/index.js
@@ -251,3 +251,20 @@ function myOutfit() {
 }
 
 myOutfit(); //task54
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+function addFive() {
+  sum += 5;
+}
+
+// Only change code below this line
+
+
+// Only change code above this line
+
+addThree();
+addFive();//task55

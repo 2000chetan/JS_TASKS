26f7493b15d79826056a49f76fa48ca9175f4dea
Chetan <chetansinghbisht@cedcoss.com>
2022-02-05 11:06:32 +0530

    task7

--- src/index.js
+++ src/index.js
@@ -18,3 +18,12 @@ b=a; //task4
 var a =9; //task5
 var myFirstName="chetan";
 var myLastName="bisht" //task6
+// Only change code below this line
+var a=5;
+var b=10;
+var c="I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!"; //task7

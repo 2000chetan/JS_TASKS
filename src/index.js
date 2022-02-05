dbf21c9ba2bbf861797ca720c0f3009fba913ecc
Chetan <chetansinghbisht@cedcoss.com>
2022-02-05 13:27:14 +0530

    task40

--- src/index.js
+++ src/index.js
@@ -154,3 +154,5 @@ var myAdverb = "quickly";
 var wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + "."; // Only change this line; //task38
 // Only change code below this line
 const myArray = ["1",2]; //task39
+// Only change code below this line
+const myArray = [["Bulls",2],["white",3]]; //task40
